2e8ba32567057fbf74bfc0315e62028f271292d5
jeyofdev <jgregoire.pro@gmail.com>
2023-04-22 18:58:21 +0200

    config : add port to .env

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,19 @@
 import { readFileSync } from 'fs';
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import 'dotenv/config';
 import { IContext, context } from './context';
 import resolvers from './resolvers';
 
+const { PORT } = process.env;
+
 const runServer = async () => {
+	if (!PORT) {
+		throw new Error(
+			'The environment variable FRONT_URL and MOBILE_URL must be specified',
+		);
+	}
+
 	const typeDefs = readFileSync('./src/schema/schema.graphql', {
 		encoding: 'utf-8',
 	});
@@ -16,7 +25,7 @@ const runServer = async () => {
 
 	const { url } = await startStandaloneServer(server, {
 		context: async () => context,
-		listen: { port: 4000 },
+		listen: { port: Number(PORT) },
 	});
 
 	// eslint-disable-next-line no-console
