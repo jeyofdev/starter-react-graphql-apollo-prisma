a7d23e6ffff390307714a42bbec3ebf2b43b1ba0
jeyofdev <jgregoire.pro@gmail.com>
2023-04-22 19:18:45 +0200

    config : add config of apollo-client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,14 @@
+import { ApolloProvider } from '@apollo/client';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
+import client from './config/apollo';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
 	</React.StrictMode>,
 );
